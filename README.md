1fca6482d87c55d8d67b93f516bd6ee4e48e80dd
Jieff Cavalcanti <jieff.cavalcanti@hotmail.com>
2023-11-01 18:58:06 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# docker run -v $(pwd):/app -p 3000:3003 aqui_nome_imagem
+# Nome do Seu Projeto
+
+O express-http-proxy é um middleware para o Express que permite criar proxies reversos, redirecionando solicitações HTTP de um servidor Express para outros serviços externos, como APIs ou recursos. É útil para rotear tráfego entre serviços e adicionar recursos de autenticação e controle de acesso.
+
+# Uso do Docker
+Você pode usar Docker para executar este projeto de forma isolada. Siga as etapas abaixo:
+
+### 1. Construir a Imagem Docker
+Execute o seguinte comando para construir a imagem Docker. Substitua [aqui_nome_imagem] pelo nome que você deseja dar à sua imagem:
+
+docker build -t [aqui_nome_imagem] .
+
+### 2. Executar o Contêiner Docker
+Execute o seguinte comando para executar o contêiner Docker. Este comando mapeia o diretório atual (onde seu projeto está localizado) para dentro do contêiner e mapeia a porta 3003 do host para a porta 3003 do contêiner. Substitua [aqui_nome_imagem] pelo nome da imagem que você criou:
+
+docker run -v $(pwd):/app -p 3003:3003 [aqui_nome_imagem]
+
+Isso iniciará o seu projeto dentro de um contêiner Docker, e você pode acessá-lo em http://localhost:3003 no seu navegador.
--- src/server.js
+++ src/server.js
@@ -8,58 +8,6 @@ const { checkToken } = require('./middleware/checkToken')
 const userServiceProxy = httpProxy(process.env.USERS_API_URL)
 const productsServiceProxy = httpProxy(process.env.PRODUCTS_API_URL)
 
-const accountsServiceProxy = httpProxy(process.env.ACCOUNTS_API_URL)
-const balanceServiceProxy = httpProxy(process.env.BALANCE_API_URL)
-const transactionServiceProxy = httpProxy(process.env.TRANSACTIONS_API_URL)
-
-router.get('/', checkToken, (req, res) => res.send('TNP Gateway'))
-// ########################
-// BALANCE ROUTE'S GETTER'S
-router.get('/api/crued', (req, res, next) =>
-  balanceServiceProxy(req, res, next)
-)
-// ########################
-// ACCOUNT ROUTE'S GETTER'S
-router.get('/api/accounts', checkToken, (req, res, next) =>
-  accountsServiceProxy(req, res, next)
-)
-router.get('/api/accounts/:id', checkToken, (req, res, next) =>
-  accountsServiceProxy(req, res, next)
-)
-// ACCOUNT ROUTE'S SETTER'S
-
-// #############################
-// TRANSACTIONS ROUTE'S GETTER'S
-router.get('/transactions', checkToken, (req, res, next) =>
-  transactionServiceProxy(req, res, next)
-)
-router.get('/user/:id/transactions', checkToken, (req, res, next) =>
-  transactionServiceProxy(req, res, next)
-)
-router.get('/seller/:id/transactions', checkToken, (req, res, next) =>
-  transactionServiceProxy(req, res, next)
-)
-
-// TRANSACTIONS ROUTE'S SETTER'S
-router.post('/user/:id/transactions/stats', checkToken, (req, res, next) =>
-  transactionServiceProxy(req, res, next)
-)
-
-router.post(
-  '/user/:id/transactions/stats/dates',
-  checkToken,
-  (req, res, next) => transactionServiceProxy(req, res, next)
-)
-
-router.post('/user/:id/transactions/operation', checkToken, (req, res, next) =>
-  transactionServiceProxy(req, res, next)
-)
-
-router.post('/transactions', checkToken, (req, res, next) =>
-  transactionServiceProxy(req, res, next)
-)
-
-
 app.get('/', (req, res) => res.send('Hello Gateway API!!!!'))
 
 app.get('/users', (req, res, next) => userServiceProxy(req, res, next))
